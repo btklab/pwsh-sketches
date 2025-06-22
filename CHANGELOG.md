f77bf1c0005d29e869bf126739019eb18303e44f
btklab <btklabggl@gmail.com>
2025-06-22 11:10:40 +0900

    Add-Comment for better maintainablity.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [unreleased]
 
-- NA
+- Added [rev2][] Comment for better maintainability.
+- Added [pwmake][] Use `@` prefix for targets as a shorthand for `.PHONY`.
+
 
 ## [0.10.0] - 2025-06-15
 
--- src/rev2_function.ps1
+++ src/rev2_function.ps1
@@ -1,63 +1,63 @@
 <#
 .SYNOPSIS
-    rev2 - Reverse columns
+    Reverse columns.
 
-    Reverse columns separated by space.
-    Do not reverse strings in columns.
-
-    Accepts only input from pipeline
-
-    Usage:
-        rev2 [-e]
-    
-    Option:
-        -e: (echo) 入力データも出力する
+    Reverses columns separated by a delimiter (default: space).
+    Does not reverse strings within columns.
+    Accepts input from the pipeline.
 
 .DESCRIPTION
-
     The main design pattern of this command was abstracted from
-    greymd(Yasuhiro, Yamada)'s "egzact",
+    greymd(Yasuhiro, Yamada)'s "egzact".
     Here is the original copyright notice for "egzact":
-    
+
         The MIT License Copyright (c) 2016 Yasuhiro, Yamada
         https://github.com/greymd/egzact/blob/master/LICENSE
 
 .LINK
     rev, rev2, stair, cycle
 
-
 .EXAMPLE
     "01 02 03" | rev2
-    03 02 01
+    # Output: 03 02 01
 
 .EXAMPLE
     "01 02 03" | rev2 -e
-    01 02 03
-    03 02 01
+    # Output:
+    # 01 02 03
+    # 03 02 01
 
 #>
 function rev2 {
     Param(
-        [parameter(Mandatory=$False)]
+        [Parameter(Mandatory=$False)]
         [Alias('e')]
-        [switch] $echo,
+        [switch] $echo, # If specified, echoes the input line before reversing.
 
-        [parameter(Mandatory=$False)]
+        [Parameter(Mandatory=$False)]
         [Alias('d')]
-        [string] $Delimiter = " ",
+        [string] $Delimiter = " ", # Delimiter for splitting columns (default is space).
 
-        [parameter(Mandatory=$False,
+        [Parameter(Mandatory=$False,
             ValueFromPipeline=$True)]
-        [string[]]$Text
+        [string[]]$Text # Input text from the pipeline.
     )
     process {
-        [string] $readLine = "$_".Trim()
-        if ( $Delimiter -eq '' ){
-            [string[]] $splitReadLine = $readLine.ToCharArray()
+        [string] $readLine = "$_".Trim() # Get current pipeline object and trim whitespace.
+
+        # Determine how to split the input line based on the delimiter.
+        if ($Delimiter -eq '') {
+            [string[]] $splitReadLine = $readLine.ToCharArray() # Split into characters if delimiter is empty.
         } else {
-            [string[]] $splitReadLine = $readLine.Split( $Delimiter )
+            [string[]] $splitReadLine = $readLine.Split($Delimiter) # Split by specified delimiter.
         }
-        if($echo){ Write-Output $readLine }
+
+        # If -echo switch is present, output the original line.
+        if ($echo) {
+            Write-Output $readLine
+        }
+
+        # Reverse the order of the split columns and join them back with the delimiter.
         [string] $writeLine = [string]::join($Delimiter, $splitReadLine[($splitReadLine.Count - 1)..0])
         Write-Output $writeLine
     }
